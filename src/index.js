653329bbe1d1dec4331035de9878f0bafc87308c
Jeremy T <jeremytupas@gmail.com>
2022-03-25 11:25:30 -0500

    Added input for projects

--- src/index.js
+++ src/index.js
@@ -10,14 +10,16 @@ const createTodo = (title, description, dueDate, priority) => {
 const header = document.querySelector('header');
 const addButton = document.querySelector('.add-button');
 const formPopup = document.querySelector('.popup-form');
-const formCloseBtn = document.querySelector('.close-form');
+const formCloseBtn = document.querySelector('.form-close');
 const formSubmitBtn = document.querySelector('#form-btn');
+const formResetBtn = document.querySelector('#form-reset');
 //Form tags
 const titleTag = document.querySelector('#title');
 const descriptionTag = document.querySelector('#description');
 const dueDateTag = document.querySelector('#due-date');
 const priorityTag = document.querySelector('#priority');
-const formTags = [titleTag, descriptionTag, dueDateTag, priorityTag];
+const projectTag = document.querySelector('#project');
+const formTags = [titleTag, descriptionTag, dueDateTag, priorityTag, projectTag]; // Loop through input and select tags
 
 const saveInfoInput = () => {
     const title = titleTag.value;
@@ -40,4 +42,20 @@ formSubmitBtn.addEventListener('click', () => {
 formCloseBtn.addEventListener('click', () => {
     formTags.forEach(tag => tag.value = null);
     formPopup.style.display = 'none';
+});
+formResetBtn.addEventListener('click', () => {
+    formTags.forEach(tag => tag.value = null);
+})
+projectTag.addEventListener('click', () => {
+    if (projectTag.value == 'add-project') {
+        while (projectTag.firstChild) {
+            projectTag.removeChild(projectTag.firstChild);
+        };
+        const newInput = document.createElement('input');
+        newInput.setAttribute('type', 'text');
+        newInput.setAttribute('id', 'project');
+        newInput.setAttribute('autocomplete', 'off');
+        projectTag.replaceWith(newInput);
+        document.querySelector(`label[for='project']`).textContent = 'Project name';
+    };
 });
