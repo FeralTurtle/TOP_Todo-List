5079e0ea56dcaa3f4ca81216429a753965441fde
Jeremy T <jeremytupas@gmail.com>
2022-03-24 22:18:10 -0500

    Todo items can be made through popup form

--- src/index.js
+++ src/index.js
@@ -9,12 +9,35 @@ const createTodo = (title, description, dueDate, priority) => {
 
 const header = document.querySelector('header');
 const addButton = document.querySelector('.add-button');
-const popupForm = document.querySelector('.popup-form');
-const closeFormBtn = document.querySelector('.close-form');
+const formPopup = document.querySelector('.popup-form');
+const formCloseBtn = document.querySelector('.close-form');
+const formSubmitBtn = document.querySelector('#form-btn');
+//Form tags
+const titleTag = document.querySelector('#title');
+const descriptionTag = document.querySelector('#description');
+const dueDateTag = document.querySelector('#due-date');
+const priorityTag = document.querySelector('#priority');
+const formTags = [titleTag, descriptionTag, dueDateTag, priorityTag];
+
+const saveInfoInput = () => {
+    const title = titleTag.value;
+    const description = descriptionTag.value;
+    const dueDate = dueDateTag.value;
+    const priority = priorityTag.value;
+
+    const newTodo = createTodo(title, description, dueDate, priority);
+    return newTodo;
+}
 
 header.textContent = 'hi';
 
 addButton.addEventListener('click', () => {
-    popupForm.style.display = "flex";
+    formPopup.style.display = 'flex';
 });
-closeFormBtn.addEventListener('click', () => popupForm.style.display = "none");
+formSubmitBtn.addEventListener('click', () => {
+    console.log(saveInfoInput());
+});
+formCloseBtn.addEventListener('click', () => {
+    formTags.forEach(tag => tag.value = null);
+    formPopup.style.display = 'none';
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -22,4 +23,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 };
